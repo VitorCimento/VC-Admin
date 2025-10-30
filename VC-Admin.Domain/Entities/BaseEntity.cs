6f08e3b0bf55cf899feb734cfbef20726a9632c8
Vitor Cimento <25249730+VitorCimento@users.noreply.github.com>
2025-10-30 16:43:33 -0300

    Ajustes nos campos de data. Adicionada padronização para definição das datas de inclusão/alteração.

--- VC-Admin.Domain/Entities/BaseEntity.cs
+++ VC-Admin.Domain/Entities/BaseEntity.cs
@@ -1,6 +1,6 @@
 ﻿namespace VC_Admin.Domain.Entities
 {
-    public class BaseEntity
+    public abstract class BaseEntity
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         public DateTime CreatedAt { get; }
--- VC-Admin.Infrastructure/Contexts/AppDbContext.cs
+++ VC-Admin.Infrastructure/Contexts/AppDbContext.cs
@@ -15,12 +15,45 @@ namespace VC_Admin.Infrastructure.Contexts
             modelBuilder.Entity<User>(entity =>
             {
                 entity.HasKey(u => u.Id);
-                entity.HasIndex(u => u.Email).IsUnique();
-                entity.Property(u => u.Email).IsRequired().HasMaxLength(200);
-                entity.Property(u => u.Username).HasMaxLength(100);
-                entity.Property(u => u.PasswordHash).IsRequired();
-                entity.Property(u => u.CreatedAt).HasDefaultValueSql("now()");
+                entity.HasIndex(u => u.Email)
+                    .IsUnique();
+                entity.Property(u => u.Email)
+                    .IsRequired()
+                    .HasMaxLength(200);
+                entity.Property(u => u.Username)
+                    .HasMaxLength(100);
+                entity.Property(u => u.PasswordHash)
+                    .IsRequired();
+                entity.Property(u => u.CreatedAt)
+                    .HasColumnType("timestamp with time zone")
+                    .HasDefaultValueSql("now()");
+                entity.Property(u => u.UpdatedAt)
+                    .HasColumnType("timestamp with time zone");
             });
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            SetEntityTimestamps();
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        public override int SaveChanges()
+        {
+            SetEntityTimestamps();
+            return base.SaveChanges();
+        }
+
+        private void SetEntityTimestamps()
+        {
+            var entries = ChangeTracker.Entries<BaseEntity>();
+            var now = DateTime.UtcNow;
+            foreach (var entry in entries)
+            {
+                var prop = entry.State == EntityState.Added ? nameof(BaseEntity.CreatedAt) : nameof(BaseEntity.UpdatedAt);
+
+                entry.Property(prop).CurrentValue = now;
+            }
         }
     }
 }
