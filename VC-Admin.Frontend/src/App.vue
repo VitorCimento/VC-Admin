5f635b484afe46778eac198e7601a67eff94d990
Vitor Cimento <25249730+VitorCimento@users.noreply.github.com>
2025-11-01 23:43:12 -0300

    Adicionando Tailwindcss e página 404.

--- VC-Admin.Frontend/src/App.vue
+++ VC-Admin.Frontend/src/App.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="min-h-screen min-w-screen bg-linear-to-br from-sky-900 via-sky-500 to-sky-50">
+    <RouterView />
+  </div>
 </template>
 
 <style scoped></style>
--- VC-Admin.Frontend/src/router/index.ts
+++ VC-Admin.Frontend/src/router/index.ts
@@ -1,8 +1,18 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import NotFound from '@/pages/shared/NotFound.vue'
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
+
+const routes: RouteRecordRaw[] = [
+  // Deixar essa rota por último. Trataiva de 404.
+  {
+    path: '/:pathWatch(.*)*',
+    name: 'notFound',
+    component: NotFound
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: routes,
 })
 
 export default router
